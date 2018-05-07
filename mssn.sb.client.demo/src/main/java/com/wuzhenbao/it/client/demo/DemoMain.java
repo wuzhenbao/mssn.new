d7a6ac8c2ac7a086025893bacf435091e4188a9e
wuzhenbao <wzb200512@126.com>
2018-05-07 22:21:34 +0800

    多线程

--- mssn.sb.client.demo/src/main/java/com/wuzhenbao/it/client/demo/DemoMain.java
+++ mssn.sb.client.demo/src/main/java/com/wuzhenbao/it/client/demo/DemoMain.java
@@ -1,5 +1,11 @@
 package com.wuzhenbao.it.client.demo;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class DemoMain {
 
 	public static void main(String[] args) {
@@ -7,8 +13,57 @@ public class DemoMain {
 //		System.out.println(response);
 //		String result = HttpClientUtil.doGet("https://127.0.0.1:8443/json");
 //		System.out.println(result);
-		String result = ClientCustomSSL.doGet("D:\\code\\keys\\tomcat.p12", "123456", "https://127.0.0.1:8443/json", null);
-		System.out.println("返回结果："+result);
+		multThreadClient();
 	}
-
+	public static void singleThreadClient() {
+		long startTime = System.currentTimeMillis();
+		for(int i = 0;i<3;i++) {
+			String result = ClientCustomSSL.doGet("D:\\code\\keys\\tomcat.p12", "123456", "https://wuzhenbao.com:8443/json", null);
+			System.out.println(Thread.currentThread().getName()+"返回结果："+result);
+		}
+		long endTime = System.currentTimeMillis();
+		System.out.println("共耗时:"+(endTime-startTime));
+	}
+	public static void multThreadClient() {
+		long startTime = System.currentTimeMillis();
+		try {
+			ExecutorService service = Executors.newCachedThreadPool();
+			Future<String> future =service.submit(new Callable<String>() {
+				public String call() {
+					String result = ClientCustomSSL.doGet("D:\\code\\keys\\tomcat.p12", "123456", "https://wuzhenbao.com:8443/json", null);
+					System.out.println(Thread.currentThread().getName()+"返回结果："+result);
+					return "yes";
+				}
+			});
+			Future<String> future1 = service.submit(new Callable<String>() {
+				public String call() {
+					String result = ClientCustomSSL.doGet("D:\\code\\keys\\tomcat.p12", "123456", "https://wuzhenbao.com:8443/json", null);
+					System.out.println(Thread.currentThread().getName()+"返回结果："+result);
+					return "success";
+				}
+			});
+			Future<String> future2 = service.submit(new Callable<String>() {
+				public String call() {
+					String result = ClientCustomSSL.doGet("D:\\code\\keys\\tomcat.p12", "123456", "https://wuzhenbao.com:8443/json", null);
+					System.out.println(Thread.currentThread().getName()+"返回结果："+result);
+					return "ok";
+				}
+			});
+			//if(future.isDone()) {
+				System.out.println("第1"+future.get());
+			//}
+			//if(future1.isDone()) {
+				System.out.println("第2"+future1.get());
+			//}
+			//if(future2.isDone()) {
+				System.out.println("第3"+future2.get());
+			//}
+		}catch(ExecutionException e) {
+			
+		}catch( InterruptedException e) {
+			
+		}
+		long endTime = System.currentTimeMillis();
+		System.out.println("共耗时:"+(endTime-startTime));
+	}
 }
